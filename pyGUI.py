fd8b65f7d1ff9ac1248d48774e5fb455236e45a4
Arun Kumar Subbiah M <62928519+TheShadowConn@users.noreply.github.com>
2023-02-12 20:32:48 +0530

    Modified Script with comments

--- pyGUI.py
+++ pyGUI.py
@@ -10,15 +10,22 @@
 
 # app.mainloop()
 
+# -----------------------------------------------------------------------------------------------------------
+# ------------------------------------------IMPORT FUNCTIONS-------------------------------------------------
+# -----------------------------------------------------------------------------------------------------------
+
 from tkinter import *
 import ttkbootstrap as tb
 from ttkbootstrap.tableview import Tableview
 from ttkbootstrap.constants import *
 from PIL import ImageTk, Image
 
-
-
 import mysql.connector
+
+
+# ------------------------------------------------------------------------------------------------------------------
+# ------------------------------------------INITIALIZATION OF APP / MYSQL-------------------------------------------
+# ------------------------------------------------------------------------------------------------------------------
 
 mydb = mysql.connector.connect(
   host="localhost",
@@ -33,6 +40,11 @@ root.geometry("1920x1080")
 colors = root.style.colors
 
 
+# -------------------------------------------------------------------------------------------------------------------
+# ------------------------------------------GETTING ALL THE TABLES FROM DB-------------------------------------------
+# -------------------------------------------------------------------------------------------------------------------
+
+
 mycursor = mydb.cursor()
 mycursor.execute(f"SHOW tables;")
 myresult = mycursor.fetchall()
@@ -41,6 +53,11 @@ for i in myresult:
     tables.append(i[0])
 print("Tables : ",tables)
 
+
+# -----------------------------------------------------------------------------------------------------------
+# ------------------------------------------SAMPLE FUNCTION-------------------------------------------------
+# -----------------------------------------------------------------------------------------------------------
+
 def insert_func():
     insertWindow = Toplevel(root)
     insertWindow.title("Insert")
@@ -48,6 +65,19 @@ def insert_func():
     new_Label = tb.Label(insertWindow,text="You Just clicked Insert")
     new_Label.pack(pady=25)
 
+
+# ------------------------------------------------------------------------------------------------------------
+# ------------------------------------------GET ALL TABLES FUNCTION-------------------------------------------
+# ------------------------------------------------------------------------------------------------------------
+    
+def get_all_tables():
+    global nice
+    nice = table_selector.get()
+    print("FROM FUNCTION : ",nice)
+    
+# -----------------------------------------------------------------------------------------------------------
+# ------------------------------------------DISPLAY TABLE FUNCTION-------------------------------------------
+# -----------------------------------------------------------------------------------------------------------
 
 def display_func():
     displayWindow = Toplevel(root)
@@ -64,21 +94,21 @@ def display_func():
     )
     dv.grid(row=0, column=0, padx=5, pady=5)
 
-    table_name = 'profilepicture'
+    table_name = nice
 
     # Collect Headers 1st
     mycursor = mydb.cursor()
     mycursor.execute(f"SHOW columns FROM {table_name}")
     myresult = mycursor.fetchall()
-    print(type(myresult))
-    l1 = []
+    # print(type(myresult))
+    l1 = [] #l1 => Column Names
     for i in myresult:
         l1.append(i[0])
 
     # Collect Data
     mycursor.execute(f"SELECT * FROM {table_name}")
     myresult = mycursor.fetchall()
-    r_set = []
+    r_set = [] #r_set => Table Data
     for i in myresult:
         r_set.append(i)
 
@@ -91,15 +121,21 @@ def display_func():
 
 
 
-# Intelsat Logo
-# intelsat_label = tb.Label(text='Intelsat',font=("Montserrat", 30), bootstyle='warning')
-# intelsat_label.pack(padx=50,pady=25)
-open_image = Image.open("F:/Coding Projects/Python Projects/nice.png")
+# -----------------------------------------------------------------------------------------------------------
+# ------------------------------------------DISPLAY WELCOME PICTURE-------------------------------------------
+# ------------------------------------------------------------------------------------------------------------
+
+open_image = Image.open("F:/Coding Projects/Python Projects/superman.png")
 resized_image = open_image.resize((500,130))
 img = ImageTk.PhotoImage(resized_image)
 panel = Label(root, image = img)
 panel.pack(side = "top", fill = "both",padx=20,pady=50)
 # panel.place(relx=0.6,rely=0.1)
+
+
+# ---------------------------------------------------------------------------------------------------
+# ------------------------------------------WELCOME LABEL-------------------------------------------
+# ---------------------------------------------------------------------------------------------------
 
 # Welcome Screen
 welcome_label = tb.Label(text='Welcome',font=("Montserrat", 20), bootstyle='info',borderwidth=1,relief="solid")
@@ -114,31 +150,28 @@ welcome_label.pack(padx=150,pady=50)
 
 # insert_button = tb.Button(text="Insert",width=35,bootstyle='warning',command=insert_func)
 
+# ---------------------------------------------------------------------------------------------------
+# ------------------------------------------TABLE SELECTOR-------------------------------------------
+# ---------------------------------------------------------------------------------------------------
 
+table_label = tb.Label(text='Select the Table',font=("Montserrat", 13), bootstyle='warning')
+table_label.pack(padx=50,pady=2)
 
 sel=tb.StringVar()
 table_selector = tb.Combobox(values=tables,textvariable=sel)
 table_selector.pack(padx=80,pady=10)
 table_selector.current()
 
-# def setter():
-#     global sel
-#     sel = sel.get()
+set_table_button = tb.Button(text='Set the table',width=3,bootstyle='danger',command=get_all_tables)
+set_table_button.pack(padx=80,pady=10)
 
+# ---------------------------------------------------------------------------------------------------
+# ------------------------------------------DISPLAY BUTTON-------------------------------------------
+# ---------------------------------------------------------------------------------------------------
 
-# set_table = tb.Button(text='Set the table',width=3,bootstyle='danger',command=setter)
-# set_table.pack(padx=80,pady=10)
-
-print("ATLAST GOT IT : ",str(sel.get()))
-
-
-# password = tb.Entry(bootstyle='info',width=50)
-# password.pack(padx=50,pady=10)
-# password.place(relx=0.62,rely=0.47)
-
-# Login Button 
-login = tb.Button(text='Display',width=35,bootstyle='primary',command=display_func)
-login.pack(padx=80,pady=20)
+# Display Button 
+display_button = tb.Button(text='Display',width=35,bootstyle='primary',command=display_func)
+display_button.pack(padx=80,pady=20)
 # login.place(relx=0.62,rely=0.55)
 
 root.mainloop()
